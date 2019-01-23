71fb7a3a5abc59ba0429a1c28f7c82b57a32b4aa
Loki Verloren <l0k18@protonmail.com>
2019-01-23 10:34:26 +0100

    working again... damned 'public' password >_<

--- run/wallet/run.go
+++ run/wallet/run.go
@@ -4,44 +4,11 @@ import (
 	"encoding/json"
 
 	"git.parallelcoin.io/pod/lib/clog"
-	w "git.parallelcoin.io/pod/module/wallet"
-	"git.parallelcoin.io/pod/module/wallet/wallet"
+	"git.parallelcoin.io/pod/module/wallet"
 )
 
 func runNode() {
 	j, _ := json.MarshalIndent(Config, "", "  ")
 	log <- cl.Trc("running with configuration:\n" + string(j))
-
-	dbDir := w.NetworkDir(Config.Wallet.AppDataDir.Value, activeNet.Params)
-	loader := wallet.NewLoader(activeNet.Params, dbDir, 250)
-
-	// Create and start HTTP server to serve wallet client connections.
-	// This will be updated with the wallet and chain server RPC client
-	// created below after each is created.
-	rpcs, legacyRPCServer, err := startRPCServers(loader)
-	if err != nil {
-		log.Errorf("Unable to create RPC servers: %v", err)
-		return err
-	}
-
-	// Create and start chain RPC client so it's ready to connect to
-	// the wallet when loaded later.
-	if !cfg.NoInitialLoad {
-		go rpcClientConnectLoop(legacyRPCServer, loader)
-	}
-
-	loader.RunAfterLoad(func(w *wallet.Wallet) {
-		startWalletRPCServices(w, rpcs, legacyRPCServer)
-	})
-
-	if !cfg.NoInitialLoad {
-		// Load the wallet database.  It must have been created already
-		// or this will return an appropriate error.
-		_, err = loader.OpenExistingWallet([]byte(cfg.WalletPass), true)
-		if err != nil {
-			log.Error(err)
-			return err
-		}
-	}
-
+	walletmain.Main(Config.Wallet)
 }
