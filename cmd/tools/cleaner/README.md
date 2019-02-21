779ae5c84afb345001366160538160478ebb9bbc
Loki Verloren <l0k18@protonmail.com>
2019-02-21 22:13:21 +0100

    now newline-splits receivers

---
+++ cmd/tools/cleaner/README.md
@@ -0,0 +1,28 @@
+# Cleaner
+
+Cleaner simply just reads in a source file, arranges the the source in the following order, grouping all root level declarations:
+
+- types
+- const
+- var
+- func
+
+It simply sorts them lexicographically inside these groups. 
+
+Constants are left as groups because of iota increments, but variables and types are all split into one per declaration.
+
+The function sorting sorts by *, receiver type and function name, in that order, and being that ASCII puts the capital letters first, the exported methods are higher up and unexported lower down, within each of the classes in the file.
+
+A key purpose of this little tool is to assist with splitting and merging source files within packages so as to reduce the amount of scrolling required, and so logically connected things are grouped together rather than hodge podge. 
+
+Variable, type and constant declarations are often tempting to mix between functions near where the data / type is used, but again, this can make it difficult to get a clear and quick gauge of the contents of a source file.
+
+I personally have a rule if I have to scroll through more than two screenfuls the source is probably getting too long anyway (and probably is a lot copypasta), and as I attempted to clean up the source in this project repository, hoping I would finish before the second coming, this tool would have saved me so much time.
+
+Long source files and huge APIs are a maintenance nightmare. Hopefully this will help you avoid that, or more quickly deal with a mess you inherited from some former C++ programmer.
+
+## Known issues
+
+- Methods without a receiver name will disappear when you run cleaner over them. Give them names, even if you aren't using the receiver. Many of these 'omit if implicit' rules in Go complicate parsing. I am not sure why exactly this one type of function slips through but the issue may be resolved at some point.
+
+- Parenthesised var and type declarations are split (so they sort properly) but due to quirks in the `dst` library (a fork of go/ast that keeps whitespace and comments together with the code they refer to) it is not simple to break the group and properly format it. Manual removal of the braces will be required.
---
+++ cmd/tools/cleaner/main.go
@@ -0,0 +1,261 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/dave/dst"
+	"github.com/dave/dst/decorator"
+)
+
+type alias int
+
+var fset = token.NewFileSet()
+
+func (
+	a *alias,
+) String() string {
+	return ""
+	// fmt.Sprintln(*a)
+}
+
+func liner(bb []byte) []byte {
+
+	// first find the beginnings of each function in the source
+	re := regexp.MustCompile("\nfunc ")
+	fi := re.FindAllIndex(bb, -1)
+	for _, x := range fi {
+	again:
+		fmt.Println(string(bb[x[1] : x[1]+3]))
+		if bb[x[1]] == '(' {
+			fmt.Println("^ receiver")
+			if bb[x[1]+1] == '\n' &&
+				bb[x[1]+2] == '\t' {
+				fmt.Println("already split")
+				rer := regexp.MustCompile(
+					"([_a-zA-Z0-9]*[ ]?[*_a-zA-Z0-9]+)")
+				rest := bb[x[1]+3:]
+				fr := rer.FindIndex(rest)
+				if rest[fr[1]] == ',' {
+					if rest[fr[1]+1] == '\n' &&
+						rest[fr[1]+2] == ')' {
+						fmt.Println("split receiver close")
+					}
+				}
+				if rest[fr[1]] == ')' {
+					bb = append(bb[:x[1]+3+fr[1]], append([]byte{',', '\n'}, bb[x[1]+3+fr[1]:]...)...)
+				}
+			} else {
+				fmt.Println("not split")
+				bb = append(bb[:x[1]+1],
+					append([]byte{'\n', '\t'}, bb[x[1]+1:]...)...)
+				goto again
+			}
+		} else {
+			fmt.Println("^ function name")
+			// re := regexp.MustCompile("[_a-zA-Z][._a-zA-Z0-9]*")
+		}
+	}
+
+	return bb
+}
+
+func main() {
+
+	bb, e := ioutil.ReadFile(os.Args[1])
+	if e != nil {
+
+		panic(e)
+	}
+	ss := string(bb)
+	bb = sorter(ss)
+	bb = liner(bb)
+	if e := ioutil.WriteFile(os.Args[1], bb, 0644); e != nil {
+		panic(e)
+	}
+}
+
+func sorter(ss string) []byte {
+
+	splittedraw := strings.Split(ss, "\n")
+	imports := []string{}
+	for i, x := range splittedraw {
+
+		if x == "import (" {
+
+			impfound := false
+			for j := i; !impfound; j++ {
+
+				imports = append(imports, splittedraw[j])
+				if splittedraw[j] == ")" {
+
+					goto imported
+				}
+			}
+		}
+	}
+
+imported:
+	file, err := decorator.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	constcounter := 0
+	unsortedDecls := make(map[string]dst.Decl)
+	for _, decl := range file.Decls {
+
+		switch gen := decl.(type) {
+
+		case *dst.GenDecl:
+
+			switch gen.Tok {
+
+			case token.CONST:
+				already := false
+				for _, x := range unsortedDecls {
+
+					if decl == x {
+						already = true
+					}
+				}
+
+				if already {
+
+					continue
+				}
+				unsortedDecls["2const"+fmt.Sprint(constcounter)] = decl
+				constcounter++
+
+			case token.VAR:
+				for _, y := range gen.Specs {
+
+					if is, ok := y.(*dst.ValueSpec); ok {
+
+						var declName string
+						key := "3:"
+						for i, z := range is.Names {
+
+							key += fmt.Sprint(z)
+							_ = i
+							declName = z.Name
+						}
+
+						d := dst.Clone(decl).(*dst.GenDecl)
+						for i, x := range decl.(*dst.GenDecl).Specs {
+
+							dd := d.Specs[i].(*dst.ValueSpec)
+							if dd.Names[0].Name == declName {
+
+								d.Specs = nil
+								d.Specs = append(d.Specs, x)
+								break
+							}
+						}
+
+						unsortedDecls[key] = d
+					}
+				}
+
+			case token.TYPE:
+				for _, y := range gen.Specs {
+
+					if is, ok := y.(*dst.TypeSpec); ok {
+
+						var declName string
+						key := "1:"
+						declName = is.Name.Name
+						d := dst.Clone(decl).(*dst.GenDecl)
+						for i, x := range decl.(*dst.GenDecl).Specs {
+
+							dd := d.Specs[i].(*dst.TypeSpec)
+							if dd.Name.Name == declName {
+
+								key += dd.Name.Name
+								d.Specs = nil
+								d.Specs = append(d.Specs, x)
+								break
+							}
+						}
+						unsortedDecls[key] = d
+					}
+				}
+			}
+
+		case *dst.FuncDecl:
+			if fun, ok := decl.(*dst.FuncDecl); ok {
+
+				if fun.Recv != nil {
+
+					for _, x := range fun.Recv.List {
+
+						var typename, star string
+						if rt, ok := x.Type.(*dst.StarExpr); ok {
+
+							star = "*"
+							typename = fmt.Sprint(rt.X)
+						} else {
+
+							typename = fmt.Sprint(x.Type)
+						}
+						for range x.Names {
+
+							unsortedDecls[fmt.Sprintf("4:(%s%s) %s", star, typename, fun.Name.Name)] = decl
+						}
+					}
+				} else {
+
+					unsortedDecls[fmt.Sprint("4:", fun.Name.Name)] = decl
+				}
+			}
+		}
+	}
+	var sortedDecls []string
+	for i := range unsortedDecls {
+
+		sortedDecls = append(sortedDecls, i)
+	}
+
+	var decls []dst.Decl
+	sort.Strings(sortedDecls)
+	for _, x := range sortedDecls {
+
+		decls = append(decls, unsortedDecls[x])
+	}
+
+	file.Decls = decls
+	var buf bytes.Buffer
+	decorator.Fprint(&buf, file)
+	output := string(buf.Bytes())
+	var splitout []string
+	splitted := strings.Split(output, "\n")
+	packagefound := false
+	if len(imports) < 1 {
+
+		packagefound = true
+	}
+	for _, x := range splitted {
+
+		splitout = append(splitout, x)
+		if !packagefound {
+
+			sss := strings.Split(x, " ")
+			if sss[0] == "package" {
+
+				splitout = append(splitout, "")
+
+				splitout = append(splitout, imports...)
+			}
+		}
+	}
+	joined := []byte(strings.Join(splitout, "\n"))
+	return joined
+}
