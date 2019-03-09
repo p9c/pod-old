477a69e20e080b7681ada3be70ed116895aa46aa
Loki Verloren <l0k18@protonmail.com>
2019-03-09 18:11:32 +0100

    ctl done but need others for test

--- app/ctl.go
+++ app/ctl.go
@@ -1,73 +1,35 @@
 package app
 
 import (
-	"git.parallelcoin.io/pod/cmd/ctl"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/urfave/cli"
+	"fmt"
+	"git.parallelcoin.io/pod/cmd/ctl"
 )
 
 var ctlConfig = ctl.Config{}
 
-var ctlFlags = []cli.Flag{
-	cli.StringFlag{
-		Name:        "rpcserver, server, s",
-		Value:       "localhost:11048",
-		Usage:       "set rpc password",
-		Destination: &ctlConfig.RPCServer,
-	},
-	cli.StringFlag{
-		Name:        "rpcusername, rpcuser, u",
-		Value:       "user",
-		Usage:       "set rpc username",
-		Destination: &ctlConfig.RPCUser,
-	},
-	cli.StringFlag{
-		Name:        "rpcpassword, rpcpass, p",
-		Value:       "pa55word",
-		Usage:       "set rpc password",
-		Destination: &ctlConfig.RPCPass,
-	},
-	cli.StringFlag{
-		Name:        "rpccert, cert, C",
-		Value:       "~/.pod/rpc.cert",
-		Usage:       "set rpc password",
-		Destination: &ctlConfig.RPCPass,
-	},
-	cli.StringFlag{
-		Name:        "proxyserver, S",
-		Value:       "localhost:9050",
-		Usage:       "set proxy server address",
-		Destination: &ctlConfig.Proxy,
-	},
-	cli.StringFlag{
-		Name:        "proxyusername, proxyuser, U",
-		Value:       "user",
-		Usage:       "set proxy username",
-		Destination: &ctlConfig.ProxyUser,
-	},
-	cli.StringFlag{
-		Name:        "proxypassword, proxypass, P",
-		Value:       "pa55word",
-		Usage:       "set proxy password",
-		Destination: &ctlConfig.ProxyPass,
-	},
-	cli.BoolFlag{
-		Name:        "tls, T",
-		Usage:       "enable tls on connections",
-		Destination: &ctlConfig.TLS,
-	},
-	cli.BoolFlag{
-		Name:        "skipverify",
-		Usage:       "disable TLS certificate verification (not recommended)",
-		Destination: &ctlConfig.TLSSkipVerify,
-	},
-	cli.BoolFlag{
-		Name:        "testnet3, testnet",
-		Usage:       "connect to testnet",
-		Destination: &ctlConfig.TestNet3,
-	},
-	cli.BoolFlag{
-		Name:        "simnet",
-		Usage:       "connect to simnet",
-		Destination: &ctlConfig.SimNet,
-	},
+func ctlHandle(c *cli.Context) error {
+	fmt.Println("running ctl")
+	if !c.IsSet("wallet") {
+		ctlConfig.Wallet = ""
+	}
+	if !c.IsSet("useproxy") {
+		ctlConfig.Proxy = ""
+	}
+	spew.Dump(ctlConfig)
+	return nil
+}
+
+func ctlHandleList(c *cli.Context) error {
+	fmt.Println("running ctl listcommands")
+	ctlConfig.ListCommands = true
+	if !c.IsSet("wallet") {
+		ctlConfig.Wallet = ""
+	}
+	if !c.IsSet("useproxy") {
+		ctlConfig.Proxy = ""
+	}
+	spew.Dump(ctlConfig)
+	return nil
 }
--- app/main.go
+++ app/main.go
@@ -6,157 +6,7 @@ import (
 	"os"
 )
 
-var App = cli.App{
-	Name:                 "pod",
-	Version:              "v0.9.9",
-	Description:          "Parallelcoin Pod Suite -- All-in-one everything for Parallelcoin!",
-	Copyright:            "Legacy portions derived from btcsuite/btcd under ISC licence. The remainder is already in your possession. Use it wisely.",
-	EnableBashCompletion: true,
-	Flags: []cli.Flag{
-		cli.StringFlag{
-			Name:   "datadir, d",
-			Value:  "~/.pod",
-			Usage:  "sets the data directory base for a pod instance",
-			EnvVar: "POD_DATADIR",
-		},
-		cli.StringFlag{
-			Name:   "loglevel, l",
-			Value:  "info",
-			Usage:  "sets the base for all subsystem logging",
-			EnvVar: "POD_LOGLEVEL",
-		},
-		cli.StringSliceFlag{
-			Name:  "subsystems, S",
-			Value: &cli.StringSlice{""},
-			Usage: "sets individual subsystems log levels, use 'help' to list available with list syntax",
-		},
-	},
-	Commands: []cli.Command{
-		{
-			Name:    "version",
-			Aliases: []string{"v"},
-			Usage:   "print version and exit",
-			Action: func(c *cli.Context) error {
-				fmt.Println(c.App.Version)
-				return nil
-			},
-		},
-		{
-			Name:    "init",
-			Aliases: []string{"i"},
-			Usage:   "resets configuration to factory",
-			Action: func(c *cli.Context) error {
-				fmt.Println("resetting configuration")
-				return nil
-			},
-		},
-		{
-			Name:    "conf",
-			Aliases: []string{"C"},
-			Usage:   "automate configuration setup for testnets etc",
-			Action: func(c *cli.Context) error {
-				fmt.Println("calling conf")
-				return nil
-			},
-		},
-		{
-			Name:    "ctl",
-			Aliases: []string{"c"},
-			Usage:   "send RPC commands to a node or wallet and print the result",
-			Subcommands: []cli.Command{
-				{
-					Name:    "help",
-					Aliases: []string{"?", "h"},
-					Usage:   "show help",
-				},
-				{
-					Name:  "listcommands, list, l",
-					Usage: "list commands available at endpoint",
-				},
-				{
-					Name:    "wallet",
-					Aliases: []string{"walletserver", "W"},
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:        "server, s",
-							Usage:       "set address of wallet server",
-							Value:       "localhost:11046",
-							Destination: &ctlConfig.Wallet,
-						},
-					},
-					Usage: "set address for wallet",
-					Action: func(c *cli.Context) error {
-						fmt.Println("starting with connection to wallet")
-						return nil
-					},
-				},
-			},
-			Flags: ctlFlags,
-			Action: func(c *cli.Context) error {
-				args := c.Args()
-				var wallet, help bool
-				var action func(*cli.Context) error
-				for _, x := range args {
-					switch x {
-					case "w", "wallet":
-						for _, y := range c.App.Commands {
-							if y.Name == "wallet" {
-								wallet = true
-								action = y.Action.(func(*cli.Context) error)
-							}
-						}
-					case "h", "help":
-						help = true
-					}
-				}
-				if wallet {
-					if help {
-						fmt.Println("help should be here")
-					} else {
-						_ = action(c)
-					}
-				}
-				return nil
-			},
-		},
-		{
-			Name:    "node",
-			Aliases: []string{"n"},
-			Usage:   "start parallelcoin full node",
-			Action: func(c *cli.Context) error {
-				fmt.Println("calling node")
-				return nil
-			},
-		},
-		{
-			Name:    "wallet",
-			Aliases: []string{"w"},
-			Usage:   "start parallelcoin wallet server",
-			Action: func(c *cli.Context) error {
-				fmt.Println("calling wallet")
-				return nil
-			},
-		},
-		{
-			Name:    "shell",
-			Aliases: []string{"s"},
-			Usage:   "start combined wallet/node shell",
-			Action: func(c *cli.Context) error {
-				fmt.Println("calling shell")
-				return nil
-			},
-		},
-		{
-			Name:    "gui",
-			Aliases: []string{"g"},
-			Usage:   "start GUI (TODO: should ultimately be default)",
-			Action: func(c *cli.Context) error {
-				fmt.Println("calling gui")
-				return nil
-			},
-		},
-	},
-}
+var App = cli.NewApp()
 
 func Main() int {
 	e := App.Run(os.Args)
@@ -164,4 +14,191 @@ func Main() int {
 		return 1
 	}
 	return 0
+}
+
+func init() {
+	*App = cli.App{
+		Name:                 "pod",
+		Version:              "v0.0.1",
+		Description:          "Parallelcoin Pod Suite -- All-in-one everything for Parallelcoin!",
+		Copyright:            "Legacy portions derived from btcsuite/btcd under ISC licence. The remainder is already in your possession. Use it wisely.",
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "datadir",
+				Value:  "~/.pod",
+				Usage:  "sets the data directory base for a pod instance",
+				EnvVar: "POD_DATADIR",
+			},
+			cli.StringFlag{
+				Name:   "loglevel",
+				Value:  "info",
+				Usage:  "sets the base for all subsystem logging",
+				EnvVar: "POD_LOGLEVEL",
+			},
+			cli.StringSliceFlag{
+				Name:  "subsystems",
+				Value: &cli.StringSlice{""},
+				Usage: "sets individual subsystems log levels, use 'help' to list available with list syntax",
+			},
+		},
+		Commands: []cli.Command{
+			{
+				Name:    "version",
+				Aliases: []string{"v"},
+				Usage:   "print version and exit",
+				Action: func(c *cli.Context) error {
+					fmt.Println(c.App.Version)
+					return nil
+				},
+			},
+			{
+				Name:    "ctl",
+				Aliases: []string{"c"},
+				Usage:   "send RPC commands to a node or wallet and print the result",
+				Action:  ctlHandle,
+				Subcommands: []cli.Command{
+					{
+						Name:    "listcommands",
+						Aliases: []string{"list", "l"},
+						Usage:   "list commands available at endpoint",
+						Action:  ctlHandleList,
+					},
+				},
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:        "rpcserver, server, s",
+						Value:       "localhost:11048",
+						Usage:       "set rpc password",
+						Destination: &ctlConfig.RPCServer,
+					},
+					cli.StringFlag{
+						Name:        "walletserver, ws",
+						Value:       "localhost:11046",
+						Usage:       "set wallet server to use",
+						Destination: &ctlConfig.Wallet,
+					},
+					cli.StringFlag{
+						Name:        "rpcusername, username, user, u",
+						Value:       "user",
+						Usage:       "set rpc username",
+						Destination: &ctlConfig.RPCUser,
+					},
+					cli.StringFlag{
+						Name:        "rpcpassword, password, pass, p",
+						Value:       "pa55word",
+						Usage:       "set rpc password",
+						Destination: &ctlConfig.RPCPass,
+					},
+					cli.StringFlag{
+						Name:        "rpccert, cert, C",
+						Value:       "~/.pod/rpc.cert",
+						Usage:       "set rpc password",
+						Destination: &ctlConfig.RPCCert,
+					},
+					cli.StringFlag{
+						Name:        "proxyserver, S",
+						Value:       "localhost:9050",
+						Usage:       "set proxy server address",
+						Destination: &ctlConfig.Proxy,
+					},
+					cli.StringFlag{
+						Name:        "proxyusername, proxyuser, U",
+						Value:       "user",
+						Usage:       "set proxy username",
+						Destination: &ctlConfig.ProxyUser,
+					},
+					cli.StringFlag{
+						Name:        "proxypassword, proxypass, P",
+						Value:       "pa55word",
+						Usage:       "set proxy password",
+						Destination: &ctlConfig.ProxyPass,
+					},
+					cli.BoolFlag{
+						Name:        "tls, T",
+						Usage:       "enable tls on connections",
+						Destination: &ctlConfig.TLS,
+					},
+					cli.BoolFlag{
+						Name:        "skipverify",
+						Usage:       "disable TLS certificate verification (not recommended)",
+						Destination: &ctlConfig.TLSSkipVerify,
+					},
+					cli.BoolFlag{
+						Name:        "testnet3, testnet",
+						Usage:       "connect to testnet",
+						Destination: &ctlConfig.TestNet3,
+					},
+					cli.BoolFlag{
+						Name:        "simnet",
+						Usage:       "connect to simnet",
+						Destination: &ctlConfig.SimNet,
+					},
+					cli.BoolFlag{
+						Name:  "useproxy, r",
+						Usage: "use configured proxy",
+					},
+					cli.BoolFlag{
+						Name:  "wallet, w",
+						Usage: "use configured proxy",
+					},
+				},
+			},
+			{
+				Name:    "init",
+				Aliases: []string{"i"},
+				Usage:   "resets configuration to factory",
+				Action: func(c *cli.Context) error {
+					fmt.Println("resetting configuration")
+					return nil
+				},
+			},
+			{
+				Name:    "conf",
+				Aliases: []string{"C"},
+				Usage:   "automate configuration setup for testnets etc",
+				Action: func(c *cli.Context) error {
+					fmt.Println("calling conf")
+					return nil
+				},
+			},
+
+			{
+				Name:    "node",
+				Aliases: []string{"n"},
+				Usage:   "start parallelcoin full node",
+				Action: func(c *cli.Context) error {
+					fmt.Println("calling node")
+					return nil
+				},
+			},
+			{
+				Name:    "wallet",
+				Aliases: []string{"w"},
+				Usage:   "start parallelcoin wallet server",
+				Action: func(c *cli.Context) error {
+					fmt.Println("calling wallet")
+					return nil
+				},
+			},
+			{
+				Name:    "shell",
+				Aliases: []string{"s"},
+				Usage:   "start combined wallet/node shell",
+				Action: func(c *cli.Context) error {
+					fmt.Println("calling shell")
+					return nil
+				},
+			},
+			{
+				Name:    "gui",
+				Aliases: []string{"g"},
+				Usage:   "start GUI (TODO: should ultimately be default)",
+				Action: func(c *cli.Context) error {
+					fmt.Println("calling gui")
+					return nil
+				},
+			},
+		},
+	}
 }
