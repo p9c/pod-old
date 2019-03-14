9767cf0ac92bff15f09b763d907d2610104f040b
Loki Verloren <l0k18@protonmail.com>
2019-03-14 05:56:45 +0100

    small godoc thing

---
+++ app/doc.go
@@ -0,0 +1,2 @@
+// This is the configuration/launch system for the several components of the parallelcoin suite
+package app
---
+++ app_old_/doc.go
@@ -0,0 +1,2 @@
+// This is the old buggy version of configuration/launch system for the several components of the parallelcoin suite
+package app_old
