5a5412f066d302169fd43b4cdefa66bf4b858b04
Loki Verloren <l0k18@protonmail.com>
2019-03-15 00:10:54 +0100

    once more with spirit

--- app/doc.go
+++ app/doc.go
@@ -1,5 +1,8 @@
 // Parallelcoin Pod is a multi-function universal binary that does all the things.
+//
 // Parallelcoin Pod
 //
 // Not really sure so testing it.
+//
+// Don't forget the paragraphs.
 package app
