7da87d59427d8d9bf2f092e0fa69d58256ed8d3b
Loki Verloren <l0k18@protonmail.com>
2019-03-15 00:09:10 +0100

    update

--- app/doc.go
+++ app/doc.go
@@ -1,4 +1,8 @@
 // Parallelcoin Pod
 //
-// This is the configuration/launch system for the several components of the parallelcoin suite
+// This is the configuration/launch system for the several components of the parallelcoin suite.
+//
+// This Might Be A Headline
+//
+// Not really sure so testing it.
 package app
